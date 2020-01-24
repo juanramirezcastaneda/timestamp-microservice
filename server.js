85c45f48832c62797b9c5b7a723b503e94cd1c8f
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-01-24 21:24:25 +0100

    Add base server file structure.

--- server.js
+++ server.js
@@ -9,3 +9,19 @@ var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser');
 var router = express.Router();
+
+var enableCORS = function (req, res, next) {
+    if (!process.env.DISABLE_XORIGIN) {
+        var allowedOrigins = ['https://marsh-glazer.gomix.me', 'https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+        var origin = req.headers.origin;
+        if (!process.env.XORIGIN_RESTRICT || allowedOrigins.indexOf(origin) > -1) {
+            console.log(req.method);
+            res.set({
+                "Access-Control-Allow-Origin": origin,
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+            });
+        }
+    }
+    next();
+};
