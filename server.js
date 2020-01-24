bfd53f599824b2d8cf13326dfef13b2c51013af7
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-01-24 21:31:28 +0100

    serve index file using express.

--- server.js
+++ server.js
@@ -25,3 +25,12 @@ var enableCORS = function (req, res, next) {
     }
     next();
 };
+
+var timeout = 10000;
+
+app.use(bodyParser.urlencoded({ extended: 'false' }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
