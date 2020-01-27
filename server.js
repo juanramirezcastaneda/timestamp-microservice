8e86a8088cdc3870e61e03a190e933417dac79ab
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-01-27 22:12:02 +0100

    Add route regex to prevent invalid dates

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var app = express();
 
 const port = process.env.PORT || "8000";
 
+app.get("/api/timestamp/:date_string([0-9]{4}-[0-9]{2}-[0-9]{2}||[0-9]{2}-[0-9]{2}-[0-9]{4})", (req, res) => {
+    res.status(200).send(`Timestamp service: ${req.params.date_string}`);
+});
+
 app.get("/", (req, res) => {
     res.status(200).send("WHATABYTE: Food For Devs");
 });
