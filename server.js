8f1f05ee1ec880e9133ef58292bd1501b0a87316
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-01-28 16:07:44 +0100

    Set the missing colons and substract one from month

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ var app = express();
 
 const port = process.env.PORT || "8000";
 
-app.get("/api/timestamp/:date_string([0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{2}-[0-9]{2}-[0-9]{4})", (req, res) => {
+app.get("/api/timestamp/:date_string([0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{2}-[0-9]{2}-[0-9]{4})?", (req, res) => {
 
-    const newDateObject = parseDateFromRegex(req.params.date_string)
-    const newDate = new Date(newDateObject.year, newDateObject.month, newDateObject.day);
+    console.log(req.params.date_string);
 
-    res.json({ "unix": newDate.getTime(), "utc": newDate.toUTCString() })
+    const newDateObject = req.params.date_string && parseDateFromRegex(req.params.date_string);
+    const newDate = newDateObject ?
+        new Date(newDateObject.year, newDateObject.month - 1, newDateObject.day) :
+        new Date();
+
+    res.json({ "unix": newDate.getTime(), "utc": newDate.toUTCString() });
 });
 
 app.get('*', (req, res) => res.json({ "error": "Invalid Date" }));
