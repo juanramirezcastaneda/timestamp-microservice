68d8a2d52c8a8ec433ce444ea8a0a19242b4dbd3
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-01-28 13:50:02 +0100

    create newDate object

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const port = process.env.PORT || "8000";
 
 app.get("/api/timestamp/:date_string([0-9]{4}-[0-9]{2}-[0-9]{2}||[0-9]{2}-[0-9]{2}-[0-9]{4})", (req, res) => {
 
-    const newDate = parseDateFromRegex(req.params.date_string)
+    const newDateObject = parseDateFromRegex(req.params.date_string)
 
+    const newDate = new Date(newDateObject.year, newDateObject.month, newDateObject.day);
     console.log(newDate);
     res.status(200).send(`Timestamp service: ${req.params.date_string}`);
 });
@@ -19,14 +20,19 @@ app.listen(port, () => {
 
 function parseDateFromRegex(dateString) {
     const regexForDate = /([0-9]{4})-([0-9]{2})-([0-9]{2})|([0-9]{2})-([0-9]{2})-([0-9]{4})/;
-
     const result = dateString.match(regexForDate);
     let day, month, year;
-    console.log(result);
+
     if (result[1] && result[1].length === 4) {
         year = Number(result[1]);
         month = Number(result[2]);
         day = Number(result[3]);
+    }
+
+    if (result[4] && result[4].length === 2) {
+        year = Number(result[6]);
+        month = Number(result[5]);
+        day = Number(result[4]);
     }
 
     return { day, month, year };
