21d1d93887298038232f0ece92a2027bdce04d12
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-01-28 13:43:08 +0100

    Add parser for valid date

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var app = express();
 const port = process.env.PORT || "8000";
 
 app.get("/api/timestamp/:date_string([0-9]{4}-[0-9]{2}-[0-9]{2}||[0-9]{2}-[0-9]{2}-[0-9]{4})", (req, res) => {
+
+    const newDate = parseDateFromRegex(req.params.date_string)
+
+    console.log(newDate);
     res.status(200).send(`Timestamp service: ${req.params.date_string}`);
 });
 
@@ -12,3 +16,17 @@ app.get('*', (req, res) => res.json({ "error": "Invalid Date" }));
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
 });
+
+function parseDateFromRegex(dateString) {
+    const regexForDate = /([0-9]{4})-([0-9]{2})-([0-9]{2})||([0-9]{2})-([0-9]{2})-([0-9]{4})/;
+
+    const result = dateString.match(regexForDate);
+    let day, month, year = null;
+    if (result[1].length === 4) {
+        year = Number(result[1]);
+        month = Number(result[2]);
+        day = Number(result[3]);
+    }
+
+    return { day: result[1] };
+}
