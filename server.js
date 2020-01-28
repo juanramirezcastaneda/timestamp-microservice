8528cc742a092ecb74096351f48d375c1249c806
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-01-28 13:55:30 +0100

    Add json response with expected signature

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ var app = express();
 
 const port = process.env.PORT || "8000";
 
-app.get("/api/timestamp/:date_string([0-9]{4}-[0-9]{2}-[0-9]{2}||[0-9]{2}-[0-9]{2}-[0-9]{4})", (req, res) => {
+app.get("/api/timestamp/:date_string([0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{2}-[0-9]{2}-[0-9]{4})", (req, res) => {
 
     const newDateObject = parseDateFromRegex(req.params.date_string)
-
     const newDate = new Date(newDateObject.year, newDateObject.month, newDateObject.day);
-    console.log(newDate);
-    res.status(200).send(`Timestamp service: ${req.params.date_string}`);
+
+    res.json({ "unix": newDate.getTime(), "utc": newDate.toUTCString() })
 });
 
 app.get('*', (req, res) => res.json({ "error": "Invalid Date" }));
