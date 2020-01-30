c1f1876b17bebeb7f6c10bd6f117cc6f9c164d16
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-01-30 11:48:45 +0100

    fix unix date

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ const port = process.env.PORT || "8000";
 
 app.get("/api/timestamp/:date_string([0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{2}-[0-9]{2}-[0-9]{4}|[0-9]{13})?", (req, res, next) => {
     const { date, month } = parseDateFromRegex(req.params.date_string);
-    if (month > 12 || month < 1) { next(); }
-    console.info(newDateObject);
-    if (date.getMonth() !== month) { next(); }
+
+    console.info(date);
+    console.info(month);
+    if (month && month > 12 || month < 1) { next(); }
+    if (month && date.getMonth() !== month) { next(); }
 
     res.json({ "unix": date.getTime(), "utc": date.toUTCString() });
 });
@@ -23,30 +25,32 @@ app.listen(port, () => {
 });
 
 function parseDateFromRegex(dateString) {
-    const regexForDate = /([0-9]{4})-([0-9]{2})-([0-9]{2})|([0-9]{2})-([0-9]{2})-([0-9]{4})|[0-9]{13}/;
-    const result = dateString.match(regexForDate);
+    const regexForDate = /^(\d{1,2})-(\d{1,2})-(\d{4})$|^(\d{1,4})-(\d{1,2})-(\d{2})$|^(\d{13})$/;
+    const result = regexForDate.exec(dateString);
     let day, month, year, unixDate;
 
-    if (result[1] && result[1].length === 4) {
+    if (result[0] && result[7].length === 13) {
+        unixDate = Number(result[7]);
+        return { date: new Date(unixDate * 1000), month: month };
+    }
+
+    if (result[0] && result[1].length === 4) {
         year = Number(result[1]);
         // To ensure index for month
-        month = Number(result[2] - 1);
+        month = Number(result[2]);
         day = Number(result[3]);
-        return { date: new Date(year, month, day), day, month, year };
+        return { date: new Date(year, month - 1, day), month: month };
     }
 
-    if (result[4] && result[4].length === 2) {
+    if (result[4] && result[4].length === 4) {
         year = Number(result[6]);
         // To ensure index for month
-        month = Number(result[5] - 1);
+        month = Number(result[5]);
         day = Number(result[4]);
-        return { date: new Date(year, month, day), day, month, year };
+        return { date: new Date(year, month - 1, day), month: month };
     }
 
-    if (result[7]) {
-        unixDate = Number(result[7]);
-        return { date: new Date(unixDate), day, month, year };
-    }
 
-    return { date: new Date(), day, month, year };
+
+    return { date: new Date(), month: month };
 }
